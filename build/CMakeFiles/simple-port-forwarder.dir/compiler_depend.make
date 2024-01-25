9ccbf646173e322ccf3143e5aa7bf6c62838c80c
Ethan Diosana <ethandiosana1@gmail.com>
2024-01-25 10:10:27 -0800

    Removed everything from before, copied over simple port forwarder.

---
+++ build/CMakeFiles/simple-port-forwarder.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for simple-port-forwarder.
+# This may be replaced when dependencies are built.
---
+++ include/server.h
@@ -0,0 +1,28 @@
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+#include <netinet/in.h>
+#include <p101_env/env.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+
+struct settings
+{
+    int                     backlog;
+    struct sockaddr_storage addr_in;
+    in_port_t               port_in;
+    struct sockaddr_storage addr_out;
+    in_port_t               port_out;
+    time_t                  min_seconds;
+    time_t                  max_seconds;
+    long                    min_nanoseconds;
+    long                    max_nanoseconds;
+    unsigned int            min_bytes;
+    unsigned int            max_bytes;
+    bool                    verbose;
+    bool                    very_verbose;
+};
+
+void run_server(const struct p101_env *env, struct p101_error *err, struct settings *sets);
+
+#endif    // SERVER_SERVER_H
